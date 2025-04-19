909355b575ac926796328af84c716e9c8aacd6db
vjoshi2324 <vishakhaj952874@gmail.com>
2025-04-19 19:58:37 -0400

    updates navigation between pages

--- src/main.py
+++ src/main.py
@@ -9,11 +9,41 @@ def onAppStart(app):
     app.stepsPerSecond = 60
     app.levelStartX = app.width
     app.levelStartY = app.groundY
+    app.currPage = 0
 
 def redrawAll(app):
-    (drawRect(0, app.groundY, app.width, app.height - app.groundY, 
-              fill = None, border = 'black'))
-    drawLevel1(app, app.levelStartX, app.levelStartY)
+    if app.currPage == 0:
+        drawHomePage(app)
+    elif app.currPage == 1:
+        drawLevelMenu(app)
+    elif app.currPage == 2:
+        (drawRect(0, app.groundY, app.width, app.height - app.groundY, 
+                fill = None, border = 'black'))
+        drawLevel1(app, app.levelStartX, app.levelStartY)
+
+def drawHomePage(app):
+    (drawRect(0, app.groundY + 20, app.width, app.height - app.groundY, 
+                fill = None, border = 'black'))
+    drawLabel('Geometry Dash 112', app.width/2, app.height/4, size = 50)
+    (drawRect(app.width/2 - 50, 250, 100, 80, fill = None, border = 'black', 
+             borderWidth = 5))
+    (drawPolygon(app.width/2 - 20, 270, app.width/2 - 20, 310, app.width/2 + 30, 
+                290, fill = None, border = 'black', borderWidth = 5))
+
+def drawLevelMenu(app):
+    drawLabel('Select a level!', app.width/2, app.height/5, size = 30)
+    
+    drawRect(100, 200, 225, 125, fill = None, border = 'black')
+    drawLabel('Level 1', 100 + 225/2, 200+ 125/2, size = 20)
+    
+    drawRect(100, 400, 225, 125, fill = None, border = 'black')
+    drawLabel('Level 2', 100 + 225/2, 400+ 125/2, size = 20)
+    
+    drawRect(475, 200, 225, 125, fill = None, border = 'black')
+    drawLabel('Level 3', 475 + 225/2, 200+ 125/2, size = 20)
+    
+    drawRect(475, 400, 225, 125, fill = None, border = 'black')
+    drawLabel('Demo Mode', 475 + 225/2, 400+ 125/2, size = 20)
 
 def drawLevel1(app, leftX, bottomY):
     drawSpike(app, leftX, bottomY)
@@ -52,9 +82,17 @@ def drawFlatSpike(app, leftX, bottomY):
 def drawBlock(app, leftX, bottomY):
     (drawRect(leftX, bottomY-app.gridSize, app.gridSize, 
               app.gridSize, fill = None, border = 'black'))
-    
+
+def onMousePress(app, mouseX, mouseY):
+    if app.currPage == 0:
+        if (app.width/2 - 50 <= mouseX <= app.width/2 + 50) and (210 <= mouseY <= 290):
+            app.currPage = 1
+    elif app.currPage == 1:
+        if (100 <= mouseX <= 325) and (200 <= mouseY <= 325):
+            app.currPage = 2
+
 def onStep(app):
-    app.levelStartX -= 10
+    app.levelStartX -= 6.5
 
 def main():
     runApp()
