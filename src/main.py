e30893f2fab1433d9b1e0e85b0c8c91cbee833e6
vjoshi2324 <vishakhaj952874@gmail.com>
2025-04-19 17:56:45 -0400

    setting up app + drawing basic obstacles

--- src/main.py
+++ src/main.py
@@ -4,10 +4,22 @@ def onAppStart(app):
     app.width = 800
     app.height = 600
     app.sqSize = 200
+    app.groundY = 400
+    app.gridSize = 40
 
 def redrawAll(app):
-    cx, cy = app.width/2, app.height/2
-    drawRect(cx, cy, app.sqSize, app.sqSize)
+    drawRect(0, app.groundY, app.width, app.height - app.groundY, fill = None, border = 'black')
+    drawSpike(app, 300, app.groundY)  
+    drawBlock(app, 340, app.groundY)
+
+
+def drawSpike(app, cx, cy):    
+    (drawPolygon(cx, cy, cx + 20, cy - 40, cx + 40, cy, 
+                fill = None, border='black'))
+    
+def drawBlock(app, cx, cy):
+    (drawRect(cx, cy-app.gridSize, app.gridSize, 
+              app.gridSize, fill = None, border = 'black'))
 
 def main():
     runApp()
