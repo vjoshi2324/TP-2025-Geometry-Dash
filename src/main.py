19ceb75dd67aa51ce2622697b53a120d9ddac84f
vjoshi2324 <vishakhaj952874@gmail.com>
2025-04-19 16:45:36 -0400

    installed cmu graphics

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 from cmu_graphics import *
 
-runApp()
+def onAppStart(app):
+    app.width = 800
+    app.height = 600
+    app.sqSize = 200
+
+def redrawAll(app):
+    cx, cy = app.width/2, app.height/2
+    drawRect(cx, cy, app.sqSize, app.sqSize)
+
+def main():
+    runApp()
+
+main()
