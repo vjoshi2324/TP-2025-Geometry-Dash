9b86c5ccf5ea8aab2d5cd3fa5e3ed4276ba5bd85
vjoshi2324 <vishakhaj952874@gmail.com>
2025-04-22 18:30:36 -0400

    moving level obstacles into app

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from cmu_graphics import *
+import copy
 from obstacles import Obstacles
 
 def onAppStart(app):
@@ -14,8 +15,25 @@ def onAppStart(app):
     app.iconCx = 250
     app.iconCy = app.groundY - app.gridSize/2
     app.iconVelocity = 0
-    app.gravity = 0.3
+    app.gravity = 0.335
     app.inAir = False
+    app.crashed = False
+    app.level1Obstacles = [Obstacles('spike', 0, app.groundY),
+        Obstacles('flat spike', app.gridSize*20, app.groundY),
+        Obstacles('spike', app.gridSize * 21, app.groundY),
+        Obstacles('spike', app.gridSize * 41, app.groundY),
+        Obstacles('spike', app.gridSize * 42, app.groundY),
+        Obstacles('block', app.gridSize * 43, app.groundY),
+        Obstacles('block', app.gridSize * 46, app.groundY, 2, 1),
+        Obstacles('block', app.gridSize * 49, app.groundY, 3, 1),
+        Obstacles('spike', app.gridSize * 69, app.groundY),
+        Obstacles('spike', app.gridSize * 70, app.groundY),
+        Obstacles('block', app.gridSize * 74, app.groundY, 1, 6),
+        Obstacles('block', app.gridSize * 83, app.groundY, 1, 13),
+        Obstacles('spike', app.gridSize * 89, app.groundY - app.gridSize),
+        Obstacles('block', app.gridSize * 99, app.groundY - app.gridSize, 1, 13),
+        Obstacles('spike', app.gridSize * 105, app.groundY -app.gridSize*2),
+        ]
 
 def redrawAll(app):
     if app.currPage == 0:
@@ -27,7 +45,8 @@ def redrawAll(app):
                 fill = None, border = 'black'))
         (drawRect(app.iconCx, app.iconCy, app.gridSize, app.gridSize, 
                   align = 'center'))
-        drawLevel1(app, app.levelStartX, app.levelStartY)
+        drawLevel1(app)
+
 
 def drawHomePage(app):
     (drawRect(0, app.groundY + 20, app.width, app.height - app.groundY, 
@@ -53,45 +72,29 @@ def drawLevelMenu(app):
     drawRect(475, 400, 225, 125, fill = None, border = 'black')
     drawLabel('Demo Mode', 475 + 225/2, 400+ 125/2, size = 20)
 
-def drawLevel1(app, leftX, bottomY):
-    level1Obstacles = [Obstacles('spike', leftX, bottomY),
-        Obstacles('flat spike', leftX+app.gridSize*20, bottomY),
-        Obstacles('spike', leftX + app.gridSize * 21, bottomY),
-        Obstacles('spike', leftX + app.gridSize * 41, bottomY),
-        Obstacles('spike', leftX + app.gridSize * 42, bottomY),
-        Obstacles('block', leftX + app.gridSize * 43, bottomY),
-        Obstacles('stack', leftX + app.gridSize * 46, bottomY,2),
-        Obstacles('stack', leftX + app.gridSize * 49, bottomY,3),
-        Obstacles('spike', leftX + app.gridSize * 69, bottomY),
-        Obstacles('spike', leftX + app.gridSize * 70, bottomY),
-        Obstacles('row', leftX + app.gridSize * 74, bottomY, 6),
-        Obstacles('row', leftX + app.gridSize * 83, bottomY, 13),
-        Obstacles('spike', leftX + app.gridSize * 89, bottomY - app.gridSize),
-        Obstacles('row', leftX + app.gridSize * 99, bottomY - app.gridSize, 13),
-        Obstacles('spike', leftX + app.gridSize * 105, bottomY -app.gridSize*2),
-        ]
-    
-    for i in range(len(level1Obstacles)):
-        currObstacle = level1Obstacles[i]
-        drawObstacle(app, currObstacle)
-
-def drawStack(app, leftX, bottomY, height):
-    for i in range(height):
-        drawBlock(app, leftX, bottomY - app.gridSize * i)
-
-def drawBlockRow(app, leftX, bottomY, width):
-    for i in range(width):
-        drawBlock(app, leftX + app.gridSize * i, bottomY)
+def drawLevel1(app):
+    for i in range(len(app.level1Obstacles)):
+        drawObstacle(app, app.level1Obstacles[i])
 
 def drawObstacle(app, obstacle):
-    if obstacle.type == 'stack':
-        for i in range(obstacle.size):
-            drawPolygon(*obstacle.blockVtxs[i], fill = None, border = 'black')
-    elif obstacle.type == 'row':
-        for i in range(obstacle.size):
-            drawPolygon(*obstacle.blockVtxs[i], fill = None, border = 'black')
-    else:
-        drawPolygon(*obstacle.vtxs, fill = None, border = 'black')
+        vtxsCopy = copy.copy(obstacle.vtxs)
+        for i in range(0, len(vtxsCopy), 2):
+            vtxsCopy[i] += app.levelStartX
+        drawPolygon(*vtxsCopy, fill = None, border = 'black')
+
+def checkCollision(app, L):
+    for obstacle in L:
+        if (app.iconCx - app.gridSize/2 < obstacle.leftX 
+            < app.iconCx + app.gridSize/2):
+            if obstacle.type == 'block':
+                if not app.inAir:
+                    if (app.iconCy + app.gridSize/2 
+                        > obstacle.bottomY - app.gridSize):
+                        app.crashed = True
+                else:
+                    if (app.iconCy + app.gridSize/2 
+                        > obstacle.bottomY - app.gridSize):
+                        app.iconCy = obstacle.bottomY - app.gridSize
 
 def onMousePress(app, mouseX, mouseY):
     if app.currPage == 0:
@@ -116,14 +119,14 @@ def onKeyHold(app, keys):
                 
 def onStep(app):
     if app.currPage > 1:
-        app.levelStartX -= 4
-    if app.inAir:
-        app.iconCy += app.iconVelocity
-        app.iconVelocity += app.gravity
-        if app.iconCy > app.groundY - app.gridSize/2:
-            app.iconCy = app.groundY - app.gridSize/2
-            app.iconVelocity = 0
-            app.inAir = False
+        app.levelStartX -= 4.25
+        if app.inAir:
+            app.iconCy += app.iconVelocity
+            app.iconVelocity += app.gravity
+            if app.iconCy > app.groundY - app.gridSize/2:
+                app.iconCy = app.groundY - app.gridSize/2
+                app.iconVelocity = 0
+                app.inAir = False
 
 def main():
     runApp()
--- src/obstacles.py
+++ src/obstacles.py
@@ -1,16 +1,19 @@
 class Obstacles:
-    def __init__(self, type, leftX, bottomY, size = 1):
+    def __init__(self, type, leftX, bottomY, height = 1, width = 1):
         self.type = type
         self.leftX = leftX
         self.bottomY = bottomY
         self.gridSize = 50
-        self.size = size
+        self.height = height
+        self.width = width
+        self.vtxs = []
         self.blockVtxs = []
         if self.type == 'block':
             self.vtxs = ([self.leftX, self.bottomY, 
-                          self.leftX, self.bottomY - self.gridSize, 
-                          self.leftX+self.gridSize, self.bottomY-self.gridSize, 
-                          self.leftX + self.gridSize, self.bottomY])
+                          self.leftX, self.bottomY - self.gridSize*self.height, 
+                          self.leftX + self.gridSize * self.width, 
+                          self.bottomY - self.gridSize * self.height, 
+                          self.leftX + self.gridSize*self.width, self.bottomY])
         elif self.type == 'flat spike':
             self.vtxs = ([self.leftX, self.bottomY, 
                           self.leftX + self.gridSize/2, 
@@ -26,28 +29,6 @@ class Obstacles:
                           self.leftX+self.gridSize, self.bottomY-self.gridSize, 
                           self.leftX + self.gridSize, 
                           self.bottomY - self.gridSize * (2/3)])
-        elif self.type == 'stack':
-            for i in range(self.size):
-                currBlockVtxs = ([self.leftX, self.bottomY - self.gridSize * i, 
-                                  self.leftX, self.bottomY-self.gridSize*(i+1),
-                                  self.leftX + self.gridSize, 
-                                  self.bottomY - self.gridSize * (i+1),
-                                  self.leftX + self.gridSize, 
-                                  self.bottomY- self.gridSize * i
-                                  ])
-                self.blockVtxs.append(currBlockVtxs)
-        elif self.type == 'row':
-            for i in range(self.size):
-                currBlockVtxs = ([self.leftX + self.gridSize * i, self.bottomY, 
-                                  self.leftX + self.gridSize * i, 
-                                  self.bottomY - self.gridSize,
-                                  self.leftX + self.gridSize * (i+1), 
-                                  self.bottomY - self.gridSize,
-                                  self.leftX + self.gridSize * (i+1), 
-                                  self.bottomY
-                                  ])
-                self.blockVtxs.append(currBlockVtxs)
-
             
             
     def __eq__(self, other):
