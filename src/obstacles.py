d5bf44299efbebfa60e1e51695ac3a4f0bc0e946
vjoshi2324 <vishakhaj952874@gmail.com>
2025-04-22 10:25:28 -0400

    update obstacles class

--- src/obstacles.py
+++ src/obstacles.py
@@ -1,1 +1,23 @@
-from cmu_graphics import *
+class Obstacles:
+    def __init__(self, type, leftX, bottomY, gridSize):
+        self.type = type
+        self.leftX = leftX
+        self.bottomY = bottomY
+        self.gridSize = gridSize
+
+    def obstacleSpecs(self):
+        if self.type == 'block':
+            vtxs = ([self.leftX, self.bottomY, self.leftX, 
+                     self.bottomY - self.gridSize, self.leftX + self.gridSize, 
+                     self.bottomY - self.gridSize, self.leftX + self.gridSize, 
+                     self.bottomY])
+        elif self.type == 'flat spike':
+            vtxs = ([self.leftX, self.bottomY, self.leftX + self.gridSize/2, 
+                     self.bottomY - self.gridSize/3, self.leftX + self.gridSize, 
+                     self.bottomY])
+        elif self.type == 'spike':
+            vtxs = ([self.leftX, self.bottomY, self.leftX + self.gridSize/2, 
+                     self.bottomY - self.gridSize, self.leftX + self.gridSize, 
+                     self.bottomY])
+
+
